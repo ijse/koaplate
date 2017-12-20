1b7ee6a51c4966f8626fc0288b92627ab41096b4
Li Yi <i@ijser.cn>
2017-12-20 18:04:14 +0800

    show hostname on page

--- server/router/index.ts
+++ server/router/index.ts
@@ -2,8 +2,11 @@ const Router = require('koa-router')
 const router = new Router()
 const config = require('config')
 
+const os = require('os')
+
 router.get('/guide(/.*)*', async (ctx:any) => {
   await ctx.render('guide', {
+    hostname: os.hostname(),
     env: config.get('env')
   })
 })
