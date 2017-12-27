f133ae0d5f09df8e8e103eb1cdd1292dff99163d
Li Yi <i@ijser.cn>
2017-12-27 18:06:31 +0800

    update prod config

--- config/production.js
+++ config/production.js
@@ -5,6 +5,7 @@ module.exports = {
     url: 'mysql://b197c83d5ec8a0:44eb8225@us-cdbr-iron-east-05.cleardb.net/heroku_28a5cb0e7c5f130?reconnect=true'
   },
   github: {
+    token: process.env.GH_TOKEN,
     clientID: '03527e62f9a8bd201034',
     clientSecret: '285dda7a6cb0f0cdc820ddc3d4ea8c56a4456472'
   },
